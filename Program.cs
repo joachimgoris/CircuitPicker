20a92f34ffbf878d21208977dc4a5580b7dd5450
Joachim Goris <gorisjoachim@hotmail.com>
2017-12-15 21:20:41 +0100

    First commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CircuitPicker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello!");
+
+            List<string> circuits = new List<string>(){"Zolder","Spa" };
+            
+            foreach (var item in circuits)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+    }
+}
