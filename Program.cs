67b5293f887f006ec445a5466dc7acf996607d89
Joachim Goris <gorisjoachim@hotmail.com>
2017-12-19 16:31:48 +0100

    Tried reading XML file

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Xml;
+using System.Xml.Linq;
 
 namespace CircuitPicker
 {
@@ -9,29 +10,50 @@ namespace CircuitPicker
         static void Main(string[] args)
         {
             Console.WriteLine("Hello!");
+            List<Circuit> circuits = new List<Circuit>();
 
             
-            XmlTextReader reader = new XmlTextReader("Circuits.xml");
-            while (reader.Read()) 
+            XElement xDocument = XElement.Load("Circuits.xml");
+            IEnumerable<XElement> c = xDocument.Elements();
+            foreach (var circuit in c)
             {
-                switch (reader.NodeType) 
-                {
-                    case XmlNodeType.Element: // The node is an element.
-                        Console.Write("<" + reader.Name);
-                        Console.WriteLine(">");
-                    break;
-                    case XmlNodeType.Text: //Display the text in each element.
-                        Console.WriteLine (reader.Value);
-                    break;
-                    case XmlNodeType. EndElement: //Display the end of the element.
-                        Console.Write("</" + reader.Name);
-                        Console.WriteLine(">");
-                    break;
-                }
+                Console.WriteLine (c);
             }
+            
 
 
             Console.ReadLine();
+        }
+    }
+
+    class Circuit
+    {
+        private string Name;
+        private string Location;
+        private List<string> Layouts;
+        
+        public string getName(){
+            return Name;
+        }
+
+        public string getLocation(){
+            return Location;
+        }
+
+        public List<string> getLayouts(){
+            return Layouts;
+        }
+
+        public void setName(string name){
+            Name = name;
+        }
+
+        public void setLocation(string location){
+            Location = location;
+        }
+
+        public void addLayout(string layout){
+            Layouts.Add(layout);
         }
     }
 }
