5d591025cff9a01a95446db642ca32d4f459b34c
Joachim Goris <gorisjoachim@hotmail.com>
2017-12-22 20:11:28 +0100

    tried fixing conflict

--- CircuitPicker/Program.cs
+++ CircuitPicker/Program.cs
@@ -13,33 +13,46 @@ namespace CircuitPicker
         static void Main(string[] args)
         {
             List<Circuit> circuits;
+            List<Car> cars;
             using (StreamReader r = new StreamReader(@"C:\Source\Repos\CircuitPicker\Circuits.json"))
             {
                 string json = r.ReadToEnd();
                 circuits = JsonConvert.DeserializeObject<List<Circuit>>(json);
             }
+            using (StreamReader r = new StreamReader(@"C:\Source\Repos\CircuitPicker\Cars.json"))
+            {
+                string json = r.ReadToEnd();
+                cars = JsonConvert.DeserializeObject<List<Car>>(json);
+            }
             Random rdm = new Random();
             string input;
             do
             {
-                int nr = rdm.Next(circuits.Count - 1);
-                StringBuilder str =
-                    new StringBuilder("Name: " + circuits[nr].Name + "\nLocation: " + circuits[nr].Location +
+                int rdmCircuit = rdm.Next(circuits.Count - 1);
+                int rdmCar = rdm.Next(cars.Count - 1);
+                string strCar;
+                if (cars[rdmCar].IsDlc == true)
+                {
+                    strCar = ("Car :" + cars[rdmCar].Brand + " " + cars[rdmCar].Type + " " + cars[rdmCar].Year +
+                                     " DLC: " + cars[rdmCar].DlcPack);
+                }
+                else
+                {
+                    strCar = ("Car :" + cars[rdmCar].Brand + " " + cars[rdmCar].Type + " " + cars[rdmCar].Year);
+                }
+
+                StringBuilder strCircuit =
+                    new StringBuilder("Name: " + circuits[rdmCircuit].Name + "\nLocation: " + circuits[rdmCircuit].Location +
                                       "\nLayout: ");
-                var layouts = circuits[nr].Layout.Split('|');
-                nr = rdm.Next(layouts.Length - 1);
-                str.Append(layouts[nr]);
-                Console.WriteLine(str.ToString());
+                var layouts = circuits[rdmCircuit].Layout.Split('|');
+                rdmCircuit = rdm.Next(layouts.Length - 1);
+                strCircuit.Append(layouts[rdmCircuit]);
+                Console.WriteLine(strCar);
+                Console.WriteLine(strCircuit.ToString());
                 input = Console.ReadLine();
                 Console.Clear();
-            } while (input !="N");
+            } while (input.ToLower() != "n");
         }
-    }
-
-    class Payload
-    {
-        [JsonProperty("circuits")]
-        public List<Circuit> Circuits { get; set; }
     }
 
     internal class Circuit
@@ -47,10 +60,26 @@ namespace CircuitPicker
         [JsonProperty("circuitId")]
         public int Id { get; set; }
         [JsonProperty("name")]
-        public  string Name { get; set; }
+        public string Name { get; set; }
         [JsonProperty("location")]
         public string Location { get; set; }
         [JsonProperty("layout")]
-        public string Layout { get; set; }       
+        public string Layout { get; set; }
+    }
+
+    internal class Car
+    {
+        [JsonProperty("carId")]
+        public int Id { get; set; }
+        [JsonProperty("brand")]
+        public string Brand { get; set; }
+        [JsonProperty("type")]
+        public string Type { get; set; }
+        [JsonProperty("year")]
+        public int Year { get; set; }
+        [JsonProperty("isDLC")]
+        public bool IsDlc { get; set; }
+        [JsonProperty("dlcPack")]
+        public string DlcPack { get; set; }
     }
 }
